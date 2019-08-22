5af2ba6b58fe272940bc603e0a165bd0a61486cf
fightingox1 <44482532+fightingox1@users.noreply.github.com>
2019-08-22 14:30:59 -0400

    Create os.py

---
+++ os.py
@@ -0,0 +1,16 @@
+a = tyler
+a1 = 1
+while a1 == "1":
+  print("hello")
+  print(a)
+  a2 = 1
+  while a2 == '1':
+  password = input("password please")
+  if password == 6007fellponycourt:
+    correct password 
+    a3 = input("1 = claculator 2 = settings 3 = loggout")
+    if a3 == "3":
+  else:
+  print("incorrect password")
+  a2 = 1
+  
